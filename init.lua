95819ee6554be5e8e61eb7620fd57d760012abf5
German III Felisarta <german.felisarta@jg-corporation.com>
2025-02-06 14:31:01 +0800

    add flash.nvim

--- init.lua
+++ init.lua
@@ -693,7 +693,7 @@ require('lazy').setup {
     opts = {},
   },
   {
-    'NStefan002/visual-surround.nvim',
+    'NSwefan002/visual-surround.nvim',
     config = function()
       require('visual-surround').setup {
         -- if set to false, the user must manually add keymaps
@@ -727,6 +727,22 @@ require('lazy').setup {
   {
     'sphamba/smear-cursor.nvim',
     opts = {},
+  },
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    opts = {
+      modes = {
+        -- a regular search with `/` or `?`
+        search = {
+          -- when `true`, flash will be activated during regular search by default.
+          -- You can always toggle when searching with `require("flash").toggle()`
+          enabled = true,
+          highlight = { backdrop = true },
+          jump = { history = true, register = true, nohlsearch = true },
+        },
+      },
+    },
   },
 }
 
