2e72570183bfeec0f4c7f28e4b93bad701f2f60b
German III Felisarta <german.felisarta@jg-corporation.com>
2025-02-18 12:48:13 +0800

    notify on erro when formatting

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -2,7 +2,7 @@ return {
   { -- Autoformat
     'stevearc/conform.nvim',
     opts = {
-      notify_on_error = false,
+      notify_on_error = true,
       format_on_save = function(bufnr) -- Format On Save with Toggling
         -- Disable with a global or buffer-local variable
         if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
