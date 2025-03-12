57cfe58a92a35d07e85e47dcece20594ef5f2551
German III Felisarta <german.felisarta@jg-corporation.com>
2025-03-12 11:08:42 +0800

    remove duplicate plugins

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,21 +1,4 @@
 return {
-  'prettier/vim-prettier', -- prettier plugin used in SE project
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    opts = {
-      notify_on_error = true,
-      format_on_save = function(bufnr) -- Format On Save with Toggling
-        -- Disable with a global or buffer-local variable
-        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-          return
-        end
-        return { timeout_ms = 500, lsp_fallback = false }
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-      },
-    },
-  },
   {
     'mfussenegger/nvim-lint', -- Asynchronous lint loader for faster and more responseive linting
     opts = {
