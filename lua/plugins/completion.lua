69bd57e7e5578ac1002bca347c2e9f79179df76c
German III Felisarta <german.felisarta@jg-corporation.com>
2025-03-13 16:02:34 +0800

    Remove ghost text due to bugs

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -32,11 +32,6 @@ return {
         -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = 'mono',
       },
-
-      completion = {
-        ghost_text = { enabled = true },
-      },
-
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
